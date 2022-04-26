1648d743921d6dd18b6c41f2141166b97e9c9cde
D3TONAT0R <gdv@swissonline.ch>
2022-04-26 09:02:35 +0200

    Delete redundant files

--- HMCon/Formats/FileFormatUtil.cs
+++
@@ -1,8 +0,0 @@
-namespace HMCon {
-	public static class FileFormatUtil {
-
-		public static bool IsPointFormat(this FileFormat format) {
-			return format.IsFormat("ASC") || format.Identifier.StartsWith("PTS");
-		}
-	}
-}
